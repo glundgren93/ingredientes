f87fd959b34f0e506623fcffbd33e945b9437ea0
Gabriel Lundgren <glundgren93@gmail.com>
2022-07-03 15:39:21 +0200

    Adding ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
 import { useState } from "react";
+import { data } from "./data/ingredients";
 import "./App.css";
+import { Ingredient } from "./models/ingredient.model";
 
 function App() {
+  const [selectedIngredient, setSelectedIngredient] = useState<Ingredient>();
+
+  function getCombinationData(combination: string) {
+    const result = data.find((ingredient) => ingredient.name === combination);
+
+    if (!result) return null;
+
+    return (
+      <div
+        onClick={() => {
+          setSelectedIngredient(result);
+        }}
+        className="mb-5 mr-5 border border-gray-400 rounded-lg"
+      >
+        <h4 className="text-center text-xl mr-3 mb-3">{result?.name}</h4>
+        {result?.image ? (
+          <img
+            className="object-scale-down h-24 w-48 shadow-lg rounded-lg hover:shadow-xl"
+            src={result?.image}
+          ></img>
+        ) : null}
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <div className="flex flex-col justify-center items-center">
-        <div className="">
+    <div className="flex flex-col h-screen w-screen justify-between">
+      <div className="flex flex-col items-center">
+        <div>
           <h1 className="text-center font-bold text-3xl mt-5 mb-3">
             🧂🧄 Ingredientes 🧅🥕
           </h1>
           <h2 className="text-center my-2 text-2xl c-yellow">
-            Descubra as combinações dos ingredientes!
+            Descubra as combinações dos ingredientes
           </h2>
         </div>
 
@@ -20,10 +47,45 @@ function App() {
           placeholder="Busque seus ingredientes"
           className="p-2 w-2/4 h-8 border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200 font-mono "
           onChange={(e) => {
-            e.stopPropagation();
+            if (e.target.value.length > 3) {
+              const ingredient = data.find((ingredient) =>
+                ingredient.name
+                  .toLocaleLowerCase()
+                  .includes(e.target.value.toLowerCase())
+              );
+              setSelectedIngredient(ingredient);
+            }
           }}
         ></input>
       </div>
+      {selectedIngredient ? (
+        <div className="flex flex-row p-2">
+          <div className="self-start border-solid border border-gray-400 rounded-lg mr-5">
+            <h3 className="p-5 text-center text-3xl">
+              {selectedIngredient.name}
+            </h3>
+            <div className="h-48  w-96">
+              <img
+                className="object-scale-down h-48 w-96 shadow-lg hover:shadow-xl"
+                src={selectedIngredient.image}
+              ></img>
+            </div>
+          </div>
+          <div className="flex flex-wrap">
+            {selectedIngredient.combinations.map((combination) => {
+              return <div>{getCombinationData(combination)}</div>;
+            })}
+          </div>
+        </div>
+      ) : null}
+      <div></div>
+      <footer className="text-center pb-4">
+        <span className="c-brown">Idealizado por </span>
+        <a href="https://github.com/glundgren93" target={"_blank"}>
+          <strong>glundgren93</strong>
+        </a>
+        .
+      </footer>
     </div>
   );
 }
--- src/models/ingredient.model.ts
+++ src/models/ingredient.model.ts
@@ -1,9 +1,62 @@
 import { Recipe } from "./recipes.model";
 
+export enum IngredientNames {
+  "FRANGO" = "Frango",
+  "ALHO" = "Alho",
+  "CEBOLA" = "Cebola",
+  "COENTRO" = "Coentro",
+  "ESTRAGÃO" = "Estragão",
+  "LOURO" = "Louro",
+  "MANJERICÃO" = "Manjericão",
+  "MANJERONA" = "Manjerona",
+  "NOZ_MOSCADA" = "Noz Moscada",
+  "ORÉGANO" = "Orégano",
+  "SALSA" = "Salsa",
+  "SALSÃO" = "Salsão",
+  "SÁLVIA" = "Sálvia",
+  "TOMILHO" = "Tomilho",
+  "COGUMELO" = "Cogumelo",
+  "LEGUMES" = "Legumes",
+  "CARNE" = "Carne",
+  "PEIXE" = "Peixe",
+  "CALDO" = "Caldo",
+  "VINAGRES" = "Vinagres",
+  "PICOLÉS" = "Picolés",
+  "SALADAS_VERDES" = "Saladas verdes",
+  "FRUTOS_DO_MAR" = "Frutos do mar",
+  "OVOS" = "Ovos",
+  "FEIJÃO" = "Feijão",
+  "MOLHOS" = "Molhos",
+  "MOLHO_PESTO" = "Molho pesto",
+  "MOLHOS_VERMELHOS" = "Molhos vermelhos",
+  "MOLHOS_BRANCOS" = "Molhos brancos",
+  "CALDOS" = "Caldos",
+  "SOPAS" = "Sopas",
+  "PIZZA" = "Pizza",
+  "SALADAS_FRIAS" = "Saladas frias",
+  "CARNES_BRANCAS" = "Carnes brancas",
+  "ALECRIM" = "Alecrim",
+  "ALHO_PORÓ" = "Alho Poró",
+  "CEBOLINHA" = "Cebolinha",
+  "ARROZ" = "Arroz",
+  "COMINHO" = "Cominho",
+  "AÇAFRÃO" = "Açafrão",
+  "PIMENTÃO" = "Pimentão",
+  "PÁPRICA" = "Páprica",
+  "SALSINHA" = "Salsinha",
+  "SALADAS" = "Saladas",
+  "MASSAS" = "Massas",
+  "BATATAS" = "Batatas",
+  "CANELA" = "Canela",
+  "PIMENTA_DO_REINO" = "Pimenta do reino",
+  "LIMÃO" = "Limão",
+  "PORCO" = "Porco",
+}
+
 export interface Ingredient {
   name: string;
   alias?: string;
-  image?: string;
+  image: string;
   combinations: string[];
-  recipes: Recipe[];
+  recipes?: Recipe[];
 }
